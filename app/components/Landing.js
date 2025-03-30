84cfd04bad7a9c51ea6c2feef284413c98d920e7
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-03-30 00:30:32 -0600

    Added dynamic route for locations

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -1,36 +1,37 @@
 "use client"
 import { useState } from "react"
+import location from "../../public/location.svg"
+import Image from "next/image"
+import Link from "next/link"
 
 const Landing = () => {
 
     const [search, setSearch] = useState(null)
     const [data, setData] = useState([])
-    
+
     const searchWeatherData = async (location) => {
-        const url = "https://api.weatherapi.com/v1/search.json?q="+location+"&key="+process.env.NEXT_PUBLIC_ANTI;
-        const options = {
+        if(location != ""){
+            const url = "https://api.weatherapi.com/v1/search.json?q="+location+"&key="+process.env.NEXT_PUBLIC_ANTI;
+            const options = {
             method: "GET",
-        };
+            };
     
         try {
             const response = await fetch(url, options);
             const result = await response.json();
             setData(result)
         } catch (error) {
-        console.error(error);
+            alert("Error getting search results")
         }
-    }
-
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
+        }
     }
 
     return (
         <div className="landing">
-            <div className="landing-title">Climapp</div>
+            <div className="landing-title">ClimApp</div>
             <input type="search" onChange={(e)=>searchWeatherData(e.target.value)} placeholder="Search for weather in your area"/>
-            <div>{data.map((res,i)=>{
-                return <div key={i}>{res.name}</div>
+            <div className="search-results">{data.map((res,i)=>{
+                return <div className="search-item" key={i}><Link href={`/details/${res.name}`}><Image alt="" src={location} width={20} height={20}/>{res.name}, {res.region}, {res.country}</Link></div>
             })}</div>
         </div>
     )
