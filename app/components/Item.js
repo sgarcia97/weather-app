1388fb38d915075d74295f94db3f230df14bc829
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-04-01 16:19:40 -0600

    Fix implemeted for hourly forecast

--- app/components/Item.js
+++ app/components/Item.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
+import Raindrops from "../../public/weather/fill/svg/raindrops.svg"
 
-const Item = ({title, unit, val="", img}) => {
+export const Item = ({title, unit, val="", img}) => {
     return(
         <div className="item">
             <div className="item-title">{title}
@@ -11,4 +12,28 @@ const Item = ({title, unit, val="", img}) => {
     )
 }
 
-export default Item
+export const Hour = ({date, icon, temp, wind}) => {
+    return(
+        <div className="hour">
+            <div>{date}</div>
+            <div className={`forecast-img ${icon}`}></div>
+            <div>{Math.round(temp)}&deg; | <span style={{color:"#999999"}}>{Math.round(wind)}&deg;</span></div>
+        </div>
+    )
+}
+
+export const Day = ({wdata}) => {
+    return(
+        <div className="forecast">
+                            <div>{wdata.date}</div>
+                        
+                            <div className={`forecast-img-medium ${wdata.icon}`}></div>
+                            <div className="h-align"><Image alt="" width={20} height={20} src={Raindrops}/>{wdata.chance}%</div>
+                            <div>{wdata.amt} mm</div>
+                            <div className="forecast-temp">
+                              <span>{Math.round(wdata.max)}</span>
+                              <span>{Math.round(wdata.min)}</span></div>
+                          </div>
+    )
+}
+
