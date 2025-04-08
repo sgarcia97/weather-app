0c11dfa8d59865a9c567c7b94aca91e37d2a9a30
glconde <georgelouie.conde@edu.sait.ca>
2025-04-07 22:53:12 -0600

    fix erroneous push

--- app/api/weather.js
+++ app/api/weather.js
@@ -43,53 +43,42 @@ export const astronomyData = async (location = "auto:ip") => {
     const result = await response.json();
     return result;
   } catch (error) {
-    alert("Error getting search results");
-  }
-};
-
-export const astronomyData = async (location = 'auto:ip') => {
-  const url = `https://api.weatherapi.com/v1/astronomy.json?q=${location}&dt=${moment().format('YYYY-MM-DD')}&key=${process.env.NEXT_PUBLIC_ANTI}`;
-  const options = { method: "GET" };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    return result; 
-  } catch (error) {
     return { astronomy: { astro: {} } };
   }
 };
 
-  
-export const marineData = async (location = 'auto:ip') => {
-  const url = `https://api.weatherapi.com/v1/marine.json?q=${location}&dt=${moment().format('YYYY-MM-DD')}&key=${process.env.NEXT_PUBLIC_ANTI}`;
+export const marineData = async (location = "auto:ip") => {
+  const url = `https://api.weatherapi.com/v1/marine.json?q=${location}&dt=${moment().format(
+    "YYYY-MM-DD"
+  )}&key=${process.env.NEXT_PUBLIC_ANTI}`;
   const options = { method: "GET" };
 
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    return result; 
+    return result;
   } catch (error) {
     return { marine: { forecast: { forecastday: [{}] } } };
   }
 };
 
-  export const parseAstronomyData = (data) => {
-    const astro = data.astro || {};
-     return {
-      sunrise: astro.sunrise || "N/A",
-      sunset: astro.sunset || "N/A",
-      moonrise: astro.moonrise || "N/A",
-      moon_phase: astro.moon_phase || "N/A",
-    };
+export const parseAstronomyData = (data) => {
+  const astro = data.astro || {};
+  return {
+    sunrise: astro.sunrise || "N/A",
+    sunset: astro.sunset || "N/A",
+    moonrise: astro.moonrise || "N/A",
+    moon_phase: astro.moon_phase || "N/A",
   };
-  
-  export const parseMarineData = (data) => {
-    const marineHour = data.forecastday?.[0]?.hour?.[0] || {};
-      return {
-      waterTemp: marineHour.water_temp_c ? `${marineHour.water_temp_c}°C` : "N/A",
-      waveHeight: marineHour.swell_height_m ? `${marineHour.swell_height_m} m` : "N/A",
-      windSpeed: marineHour.wind_kph ? `${marineHour.wind_kph} km/h` : "N/A",
-    };
+};
+
+export const parseMarineData = (data) => {
+  const marineHour = data.forecastday?.[0]?.hour?.[0] || {};
+  return {
+    waterTemp: marineHour.water_temp_c ? `${marineHour.water_temp_c}°C` : "N/A",
+    waveHeight: marineHour.swell_height_m
+      ? `${marineHour.swell_height_m} m`
+      : "N/A",
+    windSpeed: marineHour.wind_kph ? `${marineHour.wind_kph} km/h` : "N/A",
   };
-  
+};
